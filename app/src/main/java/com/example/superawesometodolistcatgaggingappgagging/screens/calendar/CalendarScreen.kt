0f4a3c27759e8c7d30cd16127ad7cf4b123b85d9
Alexandre Leconte <aleconte@dwightstudio.fr>
2025-05-16 13:26:35 +0200

    Resolving conflicts

--- app/src/main/java/com/example/superawesometodolistcatgaggingappgagging/screens/calendar/CalendarScreen.kt
+++ app/src/main/java/com/example/superawesometodolistcatgaggingappgagging/screens/calendar/CalendarScreen.kt
@@ -3,22 +3,25 @@ package com.example.superawesometodolistcatgaggingappgagging.screens.calendar
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
-import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.pager.rememberPagerState
 import coil.compose.rememberAsyncImagePainter
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.DoneAll
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
@@ -29,11 +32,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -43,6 +50,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.superawesometodolistcatgaggingappgagging.R
+import com.example.superawesometodolistcatgaggingappgagging.ui.theme.AppTheme
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.format.TextStyle
@@ -72,7 +80,11 @@ fun CalendarScreen(
                                 TextStyle.FULL_STANDALONE, Locale.ENGLISH
                             ),
                             modifier = Modifier.graphicsLayer {
-
+                                if (currentDayState.value.dayOfMonth == 1) {
+                                    alpha = 1f + viewModel.pagerState.currentPageOffsetFraction.coerceIn(-0.5f, 0f) * 2
+                                } else if (currentDayState.value.dayOfMonth == currentDayState.value.lengthOfMonth()) {
+                                    alpha = 1f - viewModel.pagerState.currentPageOffsetFraction.coerceIn(0f, 0.5f) * 2
+                                }
                             }
                         )
                     }
@@ -95,28 +107,52 @@ fun CalendarScreen(
             isRefreshing = false,
             onRefresh = {}
         ) {
-            LazyColumn(
-                modifier = Modifier.padding(innerPadding)
-            ) {
-                // Add the task list here
-                // Show Cat Media
+            if (true /* Check if there if something */) {
+                LazyColumn(
+                    modifier = Modifier.padding(innerPadding).fillMaxSize()
+                ) {
+                    // Add the task list here
+                    // Show Cat Media
 
-                item {
-                    Button(
-                        onClick = { viewModel.fetchNewCat(context) },
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text("Need some inspiration?")
+                    item {
+                        Button(
+                            onClick = { viewModel.fetchNewCat(context) },
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text("Need some inspiration?")
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+                        imageUrl?.let {
+                            Image(
+                                painter = rememberAsyncImagePainter(it),
+                                contentDescription = "Cat Image",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(300.dp),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
                     }
-                    Spacer(modifier = Modifier.height(16.dp))
-                    imageUrl?.let {
-                        Image(
-                            painter = rememberAsyncImagePainter(it),
-                            contentDescription = "Cat Image",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(300.dp),
-                            contentScale = ContentScale.Crop
+                }
+            } else {
+                Box (
+                    modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(40.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.DoneAll,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.outlineVariant,
+                            modifier = Modifier.size(100.dp),
+                        )
+                        Text(
+                            text = "All done!",
+                            style = MaterialTheme.typography.headlineLarge,
+                            color = MaterialTheme.colorScheme.outlineVariant
                         )
                     }
                 }
@@ -131,6 +167,13 @@ fun DaySelector(
     viewModel: CalendarViewModel = viewModel()
 ) {
     val animationScope = rememberCoroutineScope()
+    val hapticFeedback = LocalHapticFeedback.current
+    val currentPageState = snapshotFlow { viewModel.pagerState }
+
+    LaunchedEffect(currentPageState) {
+        hapticFeedback.performHapticFeedback(HapticFeedbackType.SegmentTick)
+        Log.i("", "????")
+    }
 
     BoxWithConstraints(
         modifier = modifier.fillMaxWidth()
@@ -198,5 +241,7 @@ fun DaySelector(
 @Composable
 @Preview
 fun CalendarScreenPreview() {
-    CalendarScreen()
+    AppTheme {
+        CalendarScreen()
+    }
 }
